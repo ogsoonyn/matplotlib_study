3875e6e49a9c81621fc16877970c8c61162ffc55
ynkmr <ynakamura027@gmail.com>
2019-09-29 11:13:04 +0900

    ドラッグドロップに対応

--- sample_kivy.py
+++ sample_kivy.py
@@ -1,6 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
+from kivy.core.window import Window
+
+import mychart
+
+import concurrent.futures
+import threading
 
 class TextWidget(Widget):
     text = StringProperty()
@@ -13,9 +19,22 @@ class TextWidget(Widget):
         self.text = 'hogehoge'
 
 class TestApp(App):
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+
     def __init__(self, **kwargs):
         super(TestApp, self).__init__(**kwargs)
-        self.title = "window title"
+        self.title = "csv2chart"
+
+        self._file = Window.bind(on_dropfile = self._on_file_drop)
+
+    def _on_file_drop(self, window, file_path):
+        #self.executor.submit(mychart.showChart(file_path.decode('utf-8')))
+        #thread = threading.Thread(target = mychart.showChart(file_path.decode('utf-8')))
+        #thread.start()
+        mychart.showChart2(file_path.decode('utf-8'))
+        print(file_path.decode('utf-8'))
+        #thread.join()
+        return
 
     def build(self):
         return TextWidget()
